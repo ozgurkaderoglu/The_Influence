9a3c76400e1244d11cc8233cc2583d19788d24f8
D3DIAdriaan <156224119+D3DIAdriaan@users.noreply.github.com>
2024-08-19 15:53:54 +0200

    Backup

---
+++ BackupDeep/DeepInfection/Intermediate/Build/Win64/x64/DeepInfection/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
+#include "E:/Unreal engines/Projects/The_Influence/DeepInfection/Intermediate/Build/Win64/x64/DeepInfection/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+#include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
---
+++ BackupDeep/DeepInfection/Intermediate/Source/DeepInfection.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class DeepInfection : ModuleRules
+{
+	public DeepInfection(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
